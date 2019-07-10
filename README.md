221c1fbe32a900d238a056f2f0894516aa815c6f
DMSalesman <dmsalesman@inventati.org>
2019-07-10 16:13:13 +0200

    Refactor and squash pylint warnings

--- README.md
+++ README.md
@@ -1,62 +1,77 @@
-# Unmark
+# unmark
 
-Unmark is a CLI tool aimed at producing a README.md file suitable for use as
-[GodMode9][1] embedded user guide. It achieves this goal by scrubbing away
-any useless MarkDown syntax, replacing links with their matching anchor text
-and applying a number of text replacements read from a `patch.json.gz` file.
+`unmark` is a CLI tool aimed at producing a `README.md` file suitable for use as [GodMode9][1] embedded user guide.
+
+It achieves this goal by scrubbing away any unneeded MarkDown syntax, replacing links with their matching anchor text and applying a number of text replacements read from a `patch.json.gz` file.
 
 -----
 
 ## Requirements
 
-To run Unmark, you need **Python >= 3.5**.
+To run `unmark`, you need **Python >= 3.5**.
 
 -----
 
 ## Installation
 
-No installation procedure is needed; simply clone or download the repository.
+No installation procedure is needed; simply clone the repository, or download `unmark.zip` from the latest release.
 
 -----
 
 ## Usage
 
-Unmark is ran by means of:
+You can use:
 
-    python unmark.py src dst
+    python unmark.py -h
 
-Where `src` is the original README.md file, and `dst` is the new file.
-Precautions are taken so that already existent files are not overwritten, thus
-Unmark will halt in such a case.
+To display the following usage message:
+
+    usage: unmark.py [-h] [-f] src dst
+    
+    Create an user manual from GodMode9's README.md file.
+    
+    positional arguments:
+      src          the original README.md file
+      dst          the user manual
+    
+    optional arguments:
+      -h, --help   show this help message and exit
+      -f, --force  overwrite the output file, if present
+
+Specifying an already existing file as `dst` will halt the script; if you don't care about the file, pass `-f` to overwrite it.
+
 
 -----
 
 ## Workflow
 
-A typical run consists of the following steps:
+Once started, `unmark` will perform the following:
 
- * Opening the input file;
- * Checking for a `patch.json.gz` file in the same path as the script;
- * Applying the replacements elencated in the abovementioned file;
- * Scrubbing any MarkDown that renders to bold text, alongside backslashes;
- * Replacing links with the corresponding anchor text;
- * Writing the modified data to a new file.
+ 1. open the input file;
+ 2. load a `patch.json.gz` file located in the same directory as the script;
+ 3. perform the replacements dictated by `patch.json.gz`, report any unmatched pattern;
+ 4. scrub any unneeded MarkDown;
+ 5. replace links with the corresponding anchor text;
+ 6. write the modified data to a new file.
+
+Any errors during steps 1 and 6 are treated as fatal errors.
+
+Any errors during steps 2 and 3 are treated as warnings.
 
 -----
 
 ## The `patch.json.gz` file
 
-This file is a `gzip`ped JSON database, containing a list of lists. Each nested
-list contains the pattern to be matched, and the data to replace the pattern
-with. This allows for precise adjustments of the README.md file, from replacing
-parts of a sentence to deleting whole lines.
+This file is a `gzip`ped JSON database, containing a list of lists.
+Each nested list contains the pattern to be matched, and the data to replace the pattern
+with.
+This allows for precise adjustments of the README.md file, from replacing parts of a sentence to deleting whole lines.
 
-Unmark will warn in case this file is missing, or in case one of the patterns
-within it cannot be matched. It is generally not recommended to run Unmark
+`unmark` will warn in case this file is missing, or in case one of the patterns within it cannot be matched.
+It is generally not recommended to run `unmark`
 without said database.
 
-To edit `patch.json.gz`, unpack it with `gzip -d` first. Once you're done,
-repack it with `gzip`.
+To edit `patch.json.gz`, unpack it with `gzip -d` first. Once you're done, repack it with `gzip`.
 
 -----
 
--- unmark.py
+++ unmark.py
@@ -1,128 +1,142 @@
-"""Remove unneccessary MarkDown and text from GodMode9 README."""
-
-__author__ = "Death Mask Salesman"
-__copyright__ = "Copyright 2018, Death Mask Salesman"
-__license__ = "BSD-3-Clause"
-__version__ = "0.1"
-
-import argparse
-import gzip
-import json
-import os
-import re
-
-ap = argparse.ArgumentParser(
-        description="Scrubber for GodMode9's README.md file."""
-)
-
-ap.add_argument(
-        "input",
-        type=str,
-        help="the README.md file"
-)
-ap.add_argument(
-        "output",
-        type=str,
-        help="where to save the modified README.md"
-)
+""" Create an user manual from GodMode9's README.md file. """
+import argparse as _argparse
+import gzip as _gzip
+import json as _json
+import os as _os
+import re as _re
+import sys as _sys
 
 
-def load_patch(patch_path):
-    """Reads the patch data if any."""
+def _exit_fatal(msg):
+    """ Print an error message to stderr and exit. """
+    print("Fatal: {0}".format(msg), file=_sys.stderr)
 
-    try:
-        with gzip.open(patch_path, "rb") as f:
-            patch_data = json.load(f)
-    except BaseException:
-        # Corrupted or non-existent patch file
-        patch_data = []
+    exit(1)
 
-    return patch_data
+
+def _print_warn(msg):
+    """ Print a warning to stderr. """
+    print("Warning: {0}".format(msg, file=_sys.stderr))
+
+
+def load_input(fname):
+    """ Load a file. """
+    with open(fname, "r") as f: # pylint: disable=invalid-name
+        return f.read()
+
+
+def load_patch(fname):
+    """ Load a patch file. """
+    with _gzip.open(fname, "rb") as f: # pylint: disable=invalid-name
+        return _json.load(f)
 
 
 def apply_patch(patch_data, data):
-    """Replaces content in data if present in patch_data."""
-
+    """ Replace strings in data based on the records in patch_data. """
     unmatched = []
-
     for pattern in patch_data:
+        # pattern[0]: original string, pattern[1]: replacement
         if data.find(pattern[0]) == -1:
             unmatched.append(pattern[0])
         else:
             data = data.replace(pattern[0], pattern[1])
 
-    for pattern in unmatched:
-        print("Warning: pattern not found: {0!r}".format(pattern))
-
-    return data
+    return (data, unmatched)
 
 
 def strip_md(data):
-    """Removes certain MarkDown from line."""
-
-    # NOTE: list a two-char string before its single-char equivalent
-    #       (i.e. ["__", "_"] ok, ["_", "__"] NOT ok)
-    # Omitting "`" for now
+    """ Remove certain MarkDown from data. """
+    # NOTE: multiple occurrences of the same char go before single occurrences
+    # ["__", "_"] ok, ["_", "__"] NOT ok
     blacklist = ["__", "**", "\\"]
-
     for pattern in blacklist:
         data = data.replace(pattern, "")
 
     return data
 
 
+def write_output(data, fname, force):
+    """ Write data to fname. """
+    mode = "w" if force else "x"
+    with open(fname, mode) as f: # pylint: disable=invalid-name
+        f.write(data)
+
+
 def replace_links(data):
-    """Replaces links with their corresponding anchor text."""
+    """ Replace links with their anchor text. """
+    for link in _re.findall(r"\[(.*?)\]\((.*?)\)", data):
+        pattern = "[{0}]({1})".format(link[0], link[1])
 
-    # Identify any link in the format [anchor](URL)
-    for pair in re.findall("\[(.*?)\]\((.*?)\)", data):
-        pattern = "[{0}]({1})".format(pair[0], pair[1])
-
-        # And replace them
-        data = data.replace(pattern, pair[0])
+        data = data.replace(pattern, link[0])
 
     return data
 
 
-def main(src, dst):
-    """Core of Unmark."""
-
-    # Safety measure
-    if os.path.exists(dst):
-        print("Fatal: {0} already exists.".format(dst))
-        exit(1)
+def main(src, dst, force):
+    """ Core function. """
+    basedir = _os.path.dirname(_os.path.abspath(__file__))
+    patch = _os.path.join(basedir, "patch.json.gz")
 
     try:
-        with open(src, "r") as f:
-            data = f.read()
-    except BaseException as e:
-        print("Fatal: {0}.".format(e))
-        exit(1)
+        data = load_input(src)
+    except FileNotFoundError:
+        _exit_fatal("{0}: no such file.".format(src))
+    except PermissionError:
+        _exit_fatal("{0}: permission denied.".format(src))
+    except IsADirectoryError:
+        _exit_fatal("{0}: is a directory.".format(src))
 
-    unmark_dir = os.path.dirname(os.path.abspath(__file__))
-    patch_path = "{0}/patch.json.gz".format(unmark_dir)
+    patch_data = []
+    try:
+        patch_data = load_patch(patch)
+    except FileNotFoundError:
+        _print_warn("{0}: no such file.".format(patch))
+    except PermissionError:
+        _print_warn("{0}: pernission denied.".format(patch))
+    except _json.decoder.JSONDecodeError:
+        _print_warn("{0}: malformed JSON file.".format(patch))
 
-    # Load the JSON containing patterns and replacements
-    patch_data = load_patch(patch_path)
-    if not patch_data:
-        print("Warning: patch not found or corrupted.")
-    else:
-        # And apply the replacements
-        data = apply_patch(patch_data, data)
+    if patch_data:
+        data, unmatched = apply_patch(patch_data, data)
+        for pattern in unmatched:
+            _print_warn("unmatched pattern: {0!r}".format(pattern))
 
-    # Strip away unused MD, then replace links with anchor texts
     data = strip_md(data)
     data = replace_links(data)
 
-    # Dump it all to the ReadMe to be baked into GM9
     try:
-        with open(dst, "w") as f:
-            f.write(data)
-    except BaseException as e:
-        print("Fatal: {0}.".format(e))
+        write_output(data, dst, force)
+    except FileExistsError:
+        _exit_fatal("{0}: file already exists. Pass -f to override.".format(dst))
+    except PermissionError:
+        _exit_fatal("{0}: permission denied.".format(dst))
+    except IsADirectoryError:
+        _exit_fatal("{0}: is a directory.".format(dst))
 
 
 if __name__ == "__main__":
-    args = ap.parse_args()
+    AP = _argparse.ArgumentParser(
+        description="Create an user manual from GodMode9's README.md file."
+    )
 
-    main(args.input, args.output)
+    AP.add_argument(
+        "src",
+        type=str,
+        help="the original README.md file"
+    )
+    AP.add_argument(
+        "dst",
+        type=str,
+        help="the user manual"
+    )
+    AP.add_argument(
+        "-f",
+        "--force",
+        default=False,
+        action="store_true",
+        help="overwrite the output file, if present"
+    )
+
+    ARGS = AP.parse_args()
+
+    main(ARGS.src, ARGS.dst, ARGS.force)
